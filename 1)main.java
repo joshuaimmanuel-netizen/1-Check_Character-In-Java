fa0c834d8730761b2a379734e423f7ec687ea483
Joshua J <106328263+joshuaimmanuel-netizen@users.noreply.github.com>
2024-06-18 18:08:47 +0530

    Add files via upload

---
+++ 1)main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+        if (Character.isUpperCase(a)) {
+            System.out.println("1");
+        }
+        else if (Character.isLowerCase(a)) {
+            System.out.println("0");
+        }
+        else {
+            System.out.println("-1");
+        }
+        sc.close();
+    }
+}
